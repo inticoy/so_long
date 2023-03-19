1f5d9be47da74fcb2e06577c7fcdd1f565616520
inticoy <inticoy0406@gmail.com>
2023-03-19 19:39:44 +0900

    Refactor assets

--- src/asset/read_assets.c
+++ src/asset/read_assets.c
@@ -6,7 +6,7 @@
 /*   By: gyoon <gyoon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/02 19:54:19 by gyoon             #+#    #+#             */
-/*   Updated: 2023/03/19 17:09:21 by gyoon            ###   ########.fr       */
+/*   Updated: 2023/03/19 19:37:30 by gyoon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,36 +26,36 @@ static void	read_blocks(void *mlx, t_assets *assets)
 
 static void	read_characters(void *mlx, t_assets *assets)
 {
-	assets->player[0][0] = read_image(mlx, "./assets/mario/left/stand.xpm");
-	assets->player[0][1] = read_image(mlx, "./assets/mario/left/walk_0.xpm");
-	assets->player[0][2] = read_image(mlx, "./assets/mario/left/walk_1.xpm");
-	assets->player[0][3] = read_image(mlx, "./assets/mario/left/walk_2.xpm");
-	assets->player[0][4] = read_image(mlx, "./assets/mario/left/slide.xpm");
-	assets->player[0][5] = read_image(mlx, "./assets/mario/left/jump.xpm");
-	assets->player[1][0] = read_image(mlx, "./assets/mario/right/stand.xpm");
-	assets->player[1][1] = read_image(mlx, "./assets/mario/right/walk_0.xpm");
-	assets->player[1][2] = read_image(mlx, "./assets/mario/right/walk_1.xpm");
-	assets->player[1][3] = read_image(mlx, "./assets/mario/right/walk_2.xpm");
-	assets->player[1][4] = read_image(mlx, "./assets/mario/right/slide.xpm");
-	assets->player[1][5] = read_image(mlx, "./assets/mario/right/jump.xpm");
-	assets->player[2][0] = read_image(mlx, "./assets/mario/dead/dead.xpm");
-	assets->player[2][1] = read_image(mlx, "./assets/mario/dead/dead.xpm");
-	assets->player[2][2] = read_image(mlx, "./assets/mario/dead/dead.xpm");
-	assets->player[2][3] = read_image(mlx, "./assets/mario/dead/dead.xpm");
-	assets->player[2][4] = read_image(mlx, "./assets/mario/dead/dead.xpm");
-	assets->player[2][5] = read_image(mlx, "./assets/mario/dead/dead.xpm");
-	assets->enemy[0] = read_image(mlx, "./assets/goomba/walk_0.xpm");
-	assets->enemy[1] = read_image(mlx, "./assets/goomba/walk_1.xpm");
-	assets->enemy[2] = read_image(mlx, "./assets/goomba/dead.xpm");
-	assets->exit = read_image(mlx, "./assets/peach/stand.xpm");
+	assets->player[0][0] = read_image(mlx, "./assets/mario/left/stand/0.xpm");
+	assets->player[0][1] = read_image(mlx, "./assets/mario/left/walk/0.xpm");
+	assets->player[0][2] = read_image(mlx, "./assets/mario/left/walk/1.xpm");
+	assets->player[0][3] = read_image(mlx, "./assets/mario/left/walk/2.xpm");
+	assets->player[0][4] = read_image(mlx, "./assets/mario/left/slide/0.xpm");
+	assets->player[0][5] = read_image(mlx, "./assets/mario/left/jump/0.xpm");
+	assets->player[1][0] = read_image(mlx, "./assets/mario/right/stand/0.xpm");
+	assets->player[1][1] = read_image(mlx, "./assets/mario/right/walk/0.xpm");
+	assets->player[1][2] = read_image(mlx, "./assets/mario/right/walk/1.xpm");
+	assets->player[1][3] = read_image(mlx, "./assets/mario/right/walk/2.xpm");
+	assets->player[1][4] = read_image(mlx, "./assets/mario/right/slide/0.xpm");
+	assets->player[1][5] = read_image(mlx, "./assets/mario/right/jump/0.xpm");
+	assets->player[2][0] = read_image(mlx, "./assets/mario/dead/0.xpm");
+	assets->player[2][1] = read_image(mlx, "./assets/mario/dead/0.xpm");
+	assets->player[2][2] = read_image(mlx, "./assets/mario/dead/0.xpm");
+	assets->player[2][3] = read_image(mlx, "./assets/mario/dead/0.xpm");
+	assets->player[2][4] = read_image(mlx, "./assets/mario/dead/0.xpm");
+	assets->player[2][5] = read_image(mlx, "./assets/mario/dead/0.xpm");
+	assets->enemy[0] = read_image(mlx, "./assets/goomba/left/0.xpm");
+	assets->enemy[1] = read_image(mlx, "./assets/goomba/right/0.xpm");
+	assets->enemy[2] = read_image(mlx, "./assets/goomba/dead/0.xpm");
+	assets->exit = read_image(mlx, "./assets/peach/stand/0.xpm");
 }
 
 static void	read_items(void *mlx, t_assets *assets)
 {
-	assets->item[0] = read_image(mlx, "./assets/coin/0.xpm");
-	assets->item[1] = read_image(mlx, "./assets/coin/1.xpm");
-	assets->item[2] = read_image(mlx, "./assets/coin/2.xpm");
-	assets->item[3] = read_image(mlx, "./assets/heart/heart.xpm");
+	assets->item[0] = read_image(mlx, "./assets/item/coin/0.xpm");
+	assets->item[1] = read_image(mlx, "./assets/item/coin/1.xpm");
+	assets->item[2] = read_image(mlx, "./assets/item/coin/2.xpm");
+	assets->item[3] = read_image(mlx, "./assets/item/heart/0.xpm");
 }
 
 t_assets	read_assets(void *mlx)
